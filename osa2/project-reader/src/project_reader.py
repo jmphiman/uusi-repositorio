7156cb0033fc9b6d4245ff7811aa3ec309af2c3b
Janne Himanen <jmphiman@gmail.com>
2025-11-09 20:00:47 +0200

    Print-statementtien poisto

--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -9,11 +9,10 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        #print(content)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
         toml_dict = tomli.loads(content)
-        print(toml_dict)
         name = toml_dict["tool"]["poetry"]["name"]
         authors = toml_dict["tool"]["poetry"]["authors"]
         desc = toml_dict["tool"]["poetry"]["description"]
